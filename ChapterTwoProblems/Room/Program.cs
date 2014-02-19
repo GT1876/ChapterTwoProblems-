b9250600d66da1b1bc462a3c28997cc9cb097044
GT1876 <garrysontime@gmail.com>
2014-02-19 10:09:09 -0500

    Hi

---
+++ ChapterTwoProblems/Room/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Room
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 15, witdth = 25, area;
+            Console.WriteLine = ("the
+        }
+    }
+}
