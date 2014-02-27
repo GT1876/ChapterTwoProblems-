f1a063ee195ca2870143ae3579cf1b82049b59a5
BCorr17 <CorrBradley@gmail.com>
2014-02-27 09:52:39 -0500

    Js on ma feet

---
+++ ChapterTwoProblems/Payroll/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Payroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 3.50; 
+        }
+    }
+}
