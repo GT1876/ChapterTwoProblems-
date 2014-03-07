8085d90ce1e92f14b29e65352f9b4704662ffb9f
GT1876 <garrysontime@gmail.com>
2014-03-07 10:11:04 -0500

    gkifdg

--- ChapterTwoProblems/Payroll/Program.cs
+++ ChapterTwoProblems/Payroll/Program.cs
@@ -9,7 +9,45 @@ namespace Payroll
     {
         static void Main(string[] args)
         {
-            double price = 3.50; 
+
+            String name, ssn, hpr, hw;
+            int hourspay, hoursworked;
+
+            double gross, fed, state, moneyperch;
+
+            Console.Write("Enter your name:");
+            name = Console.ReadLine();
+
+            Console.Write("Social Security number:");
+            ssn = Console.ReadLine();
+
+            Console.Write("Hourly pay rate:");
+            hpr = Console.ReadLine();
+            hourspay = Convert.ToInt32(hpr); 
+
+            Console.Write("Hours worked:");
+            hw = Console.ReadLine();
+            hoursworked = Convert.ToInt32(hw);
+
+            gross = hourspay * hoursworked;
+            moneyperch = gross / hoursworked;
+            fed = gross * 0.15;
+            state = gross * 0.05;
+
+            Console.WriteLine();
+           
+            Console.Write("Payroll Summary for: {0}", name);
+            name = Console.ReadLine();
+
+            Console.WriteLine("SSN {0}:", ssn);
+
+            Console.WriteLine("Ypu earned {0} at {1} per hour", gross.ToString("C"), moneyperch.ToString("C"));
+
+            Console.WriteLine("Federal withholding is: {0}", fed.ToString("C"));
+
+            Console.WriteLine("State withholding is: {0}", state.ToString("C"));
+        
         }
+
     }
 }
